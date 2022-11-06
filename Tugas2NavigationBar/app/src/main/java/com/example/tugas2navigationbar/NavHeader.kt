0d2eccc34a650a6e913d0b60a86be878999a6a60
Muhammad Shidqi Mahmudin <shidqimuhammad91@gmail.com>
2022-11-06 23:56:35 +0700

    Tugas 2

---
+++ Tugas2NavigationBar/app/src/main/java/com/example/tugas2navigationbar/NavHeader.kt
@@ -0,0 +1,11 @@
+package com.example.tugas2navigationbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavHeader : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_header)
+    }
+}
